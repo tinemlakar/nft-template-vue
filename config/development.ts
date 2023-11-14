261d4791a04b742159d2c55a5152233712973551
tinemlakar <46408582+tinemlakar@users.noreply.github.com>
2023-11-14 13:46:14 +0100

    Update development.ts

--- config/development.ts
+++ config/development.ts
@@ -1,7 +1,7 @@
 // Moonbase
 const config: ConfigInterface = {
   CHAIN_ID: '0x507',
-  CONTRACT_ADDRESS: '',
+  CONTRACT_ADDRESS: '0x8955E09bFfE741c13C31590Fc16cEe58A7280064',
 };
 // Moonbeam
 // const config: ConfigInterface = {
